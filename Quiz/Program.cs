cd06e2a57ea959e9c48715447a5b0abd485529d4
Cas Ros <16094190@student.hhs.nl>
2017-11-17 11:54:41 +0100

    fixed project klasses

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -6,7 +6,27 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
+            Question first = new Question();
+            first.setText("Who was the inventor of Java?");
+            first.setAnswer("James Gosling");
+
+            presentQuestion(first);
+
+
+
+            
+
+            
+
             Console.WriteLine("Hello World!");
+        }
+
+        public static void presentQuestion(Question q)
+        {
+            q.display();
+            Console.WriteLine("Your awnser: ");
+            string response = Console.ReadLine();
+            Console.WriteLine(q.checkAnswer(response));
         }
     }
 }
---
+++ Quiz/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Quiz
+{
+    internal class Question
+    {
+        private string text;
+        private string answer;
+
+        public string Answer { set => answer = value; }
+        public string Text { set => text = value; }
+
+        internal void display()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool checkAnswer(string response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
